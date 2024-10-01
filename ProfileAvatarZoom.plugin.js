e3d3b6088e1421bea45698829b2b93b8c528fa3d
Oleksii <oleksiiveriemieiev@gmail.com>
2024-10-01 19:33:22 +0200

    Create ProfileAvatarZoom.plugin.js

---
+++ ProfileAvatarZoom.plugin.js
@@ -0,0 +1,68 @@
+/**
+ * @name ProfileAvatarContextMenu
+ * @version 1.0.0
+ * @description Плагин для просмотра аватара в Discord
+ * @author salyamiii
+ * @authorId 528185437399810081
+ */
+
+(() => {
+    "use strict";
+
+    const { Webpack, ContextMenu } = BdApi;
+    const { Filters } = Webpack;
+    const UserStore = Webpack.getModule(Filters.byProps("getUser"));
+
+    class ShowAvatarLink {
+        start() {
+            this.patchUserContextMenu();
+        }
+
+        stop() {
+            this.unpatchUserContextMenu();
+        }
+
+        patchUserContextMenu() {
+            ContextMenu.patch("user-context", this.handleUserContextMenu);
+        }
+
+        unpatchUserContextMenu() {
+            ContextMenu.unpatch("user-context", this.handleUserContextMenu);
+        }
+
+        appendAvatarLinkMenuGroup(menu, userId) {
+            const user = UserStore.getUser(userId);
+            if (user && user.avatar) {
+                const avatarUrl = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`;
+                menu.props.children.splice(
+                    menu.props.children.length - 1,
+                    0,
+                    this.buildAvatarLinkMenuGroup(avatarUrl)
+                );
+            }
+        }
+
+        buildAvatarLinkMenuGroup(avatarUrl) {
+            return (
+                BdApi.React.createElement(ContextMenu.Group, null,
+                    BdApi.React.createElement(ContextMenu.Item, {
+                        id: "show-avatar-link",
+                        label: "Получить ссылку на аватар",
+                        action: () => this.showAvatarLink(avatarUrl),
+                        disabled: !avatarUrl,
+                    })
+                )
+            );
+        }
+
+        showAvatarLink(avatarUrl) {
+            BdApi.alert("Avatar URL", `Ссылка на аватар URL: \n\n${avatarUrl}`);
+        }
+
+        handleUserContextMenu = (menu, { user }) => {
+            this.appendAvatarLinkMenuGroup(menu, user.id);
+        };
+    }
+
+    module.exports = ShowAvatarLink;
+})();
